7e5c3cb0d054790556e16255431aac6a8c51bf54
ndr0n <esquilofreniko@gmail.com>
2025-10-03 22:48:17 +0100

    Updates

--- Entity/Scripts/EntityComponentType.cs
+++ Entity/Scripts/EntityComponentType.cs
@@ -10,46 +10,8 @@ namespace Sentience
     [System.Serializable]
     public class EntityComponentType
     {
-        // public enum ComponentType
-        // {
-        //     Item,
-        //     Spawn,
-        //     Inventory,
-        //     Health,
-        //     Attributes,
-        //     Identity,
-        //     Persona,
-        //     Lock,
-        //     Journal,
-        //     Effects,
-        //     Weapon,
-        //     Equipment,
-        //     Explosive,
-        //     Body,
-        //     Pawn,
-        //     Npc,
-        //     Player,
-        // }
-
-        // public ComponentType Component;
         [HideInInspector] public string Name;
         [SerializeReference] public EntityAuthoring Authoring;
-
-        // public EntityAuthoring SpawnAuthoringComponent(ComponentType componentType)
-        // {
-        //     EntityAuthoring component = null;
-        //     Type baseType = typeof(EntityAuthoring);
-        //     List<Type> authoringTypes = EntityLibrary.FindDerivedTypes(baseType.Assembly, baseType).ToList();
-        //     foreach (var authoringType in authoringTypes)
-        //     {
-        //         if (authoringType.Name.Split('.')[^1].Replace("Authoring", "") == Component.ToString())
-        //         {
-        //             component = Activator.CreateInstance(authoringType) as EntityAuthoring;
-        //             break;
-        //         }
-        //     }
-        //     return component;
-        // }
 
         public EntityComponentType(string name, EntityAuthoring authoring)
         {
