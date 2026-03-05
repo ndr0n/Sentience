578793719d271b344cbc470bdf87a72fbd2ba149
ndr0n <esquilofreniko@gmail.com>
2026-03-05 18:24:16 +0000

    Keycode Implementation

--- Entity/Component/Lock.cs
+++ Entity/Component/Lock.cs
@@ -9,7 +9,7 @@ namespace Sentience
     [System.Serializable]
     public class Lock : EntityComponent
     {
-        public int KeyCode = -1;
+        public List<int> KeyCode = new();
         public List<EntityData> KeyItems = new();
 
         [SerializeField] bool open;
@@ -51,7 +51,7 @@ namespace Sentience
     [System.Serializable]
     public class LockAuthoring : EntityAuthoring
     {
-        public int KeyCode = -1;
+        public List<int> KeyCode = new();
         public List<EntityType> KeyItems = new();
         public bool Open = false;
         public bool Locked = false;
@@ -63,8 +63,7 @@ namespace Sentience
             foreach (var key in KeyItems) lockComponent.KeyItems.Add(new(key.Name, key.Description, key, random));
             lockComponent.Open = Open;
             lockComponent.Locked = Locked;
-            if (KeyCode < 0) lockComponent.KeyCode = random.Next(1000, 10000);
-            else lockComponent.KeyCode = KeyCode;
+            lockComponent.KeyCode = KeyCode;
             return lockComponent;
         }
     }
